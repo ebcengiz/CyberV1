eedeb38b885e6c936fe904d334f4b96303e6c6fd
ebcengiz <enesbugracengiz@gmail.com>
2024-06-24 17:12:38 +0300

    Created container structure on homeAv4.css and HomeAv4.jsx files and edited their css.

--- CyberV1/src/components/homeA/homeAv4/HomeAv4.jsx
+++ CyberV1/src/components/homeA/homeAv4/HomeAv4.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./homeAv4.css";
 const HomeAv4 = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
+      <Container className="homeAv4">
+        <Col>
+          <Row className="homeAv4-tags">1 of 1</Row>
+        </Col>
       </Container>
     </>
   );
