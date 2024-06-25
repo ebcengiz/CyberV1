69fe8abefdae0e97d9e84106d295d3e214104b61
ebcengiz <enesbugracengiz@gmail.com>
2024-06-25 17:06:17 +0300

    I made edits in HomeAvR2.jsx, homeAv4R2.css, HomeAv4.jsx and homeAv4.css files, created the card structure, pulled the data via db.json and made css edits.

--- CyberV1/src/components/homeA/homeAv4/HomeAv4.jsx
+++ CyberV1/src/components/homeA/homeAv4/HomeAv4.jsx
@@ -1,22 +1,51 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./homeAv4.css";
 import HomeAv4R1 from "./homeAv4R1/HomeAv4R1";
+import HomeAv4R2 from "./homeAv4R2/HomeAv4R2";
+import axios from "axios";
+
 const HomeAv4 = () => {
+  const [productsE, setProductsE] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => {
+        setProductsE(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-      <Container className="homeAv4">
-        <Col>
-          <Row className="homeAv4-tags">
-            <HomeAv4R1 />
-          </Row>
-          <Row className="homeAv4-products-grid"></Row>
-        </Col>
-      </Container>
-    </>
+    <Container className="homeAv4">
+      <Row className="homeAv4-tags">
+        <HomeAv4R1 />
+      </Row>
+      <Row className="homeAv4-products-grid">
+        {productsE.map((product) => (
+          <Col
+            key={product.id}
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            className="homeAv4-products-grid-col"
+          >
+            <HomeAv4R2
+              descP={product.descP}
+              imgP={product.imgPe}
+              price={product.priceP}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
--- CyberV1/src/components/homeA/homeAv4/homeAv4R2/HomeAv4R2.jsx
+++ CyberV1/src/components/homeA/homeAv4/homeAv4R2/HomeAv4R2.jsx
@@ -1,9 +1,49 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
 import "./homeAv4R2.css";
+import Card from "react-bootstrap/Card";
+import Figure from "react-bootstrap/Figure";
+import styled from "styled-components";
+import FavoriteIconP from "../../../../../public/FavoriteIconP.svg";
 
-const HomeAv4R2 = () => {
-  return <></>;
+const StyledButton = styled.button`
+  background-color: black;
+  border: none;
+  font-family: "SF Pro Display", sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  transition: background-color 0.3s ease;
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+  margin-top: auto;
+
+  &:hover {
+    background-color: #575757;
+  }
+`;
+
+const HomeAv4R2 = ({ descP, imgP, price }) => {
+  return (
+    <Card className="homeav4r2pc">
+      <Figure className="favorite-icon">
+        <Figure.Image
+          width={32}
+          height={32}
+          alt="Favorite Icon"
+          src={FavoriteIconP}
+        />
+      </Figure>
+      <Card.Img variant="top" src={imgP} className="homeav4r2pc-img" />
+      <Card.Body className="homeav4r2pc-body">
+        <Card.Text className="homeav4r2pc-desc">{descP}</Card.Text>
+        <Card.Text className="homeav4r2pc-price">{price}</Card.Text>
+        <StyledButton className="styled-button">Buy Now</StyledButton>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default HomeAv4R2;
