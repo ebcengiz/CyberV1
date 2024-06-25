08d643bc97e37924a4a430358e0d92b0aa124687
ebcengiz <enesbugracengiz@gmail.com>
2024-06-25 23:42:43 +0300

    HomeAv5R1.jsx file has been integrated into HomeAv5.jsx file.

--- CyberV1/src/components/homeA/homeAv5/HomeAv5.jsx
+++ CyberV1/src/components/homeA/homeAv5/HomeAv5.jsx
@@ -4,6 +4,7 @@ import "./homeAv5.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import HomeAv5R1 from "./homeAv5R1/HomeAv5R1";
 
 const HomeAv5 = () => {
   return (
@@ -11,7 +12,7 @@ const HomeAv5 = () => {
       <Container className="homeAv5Banners">
         <Row>
           <Col className="d-flex flex-column justify-content-center align-items-center">
-            1 of 1
+            <HomeAv5R1 />
           </Col>
           <Col>1 of 1</Col>
           <Col>1 of 1</Col>
