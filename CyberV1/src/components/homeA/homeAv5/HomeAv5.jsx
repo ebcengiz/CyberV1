84671559fb14937ddb92f9de9bc8f85cca8771f0
ebcengiz <enesbugracengiz@gmail.com>
2024-07-03 21:33:31 +0300

    Added keyId data to the productsB section of the db.json file. A card structure was created in the HomeAv5R1.jsx file and the data from the HomeAv5.jsx file was integrated into the card structure. css edits were made on the homeAv5R1.css side. In the HomeAv5.jsx file, the data was pulled with axios, the container structure was created and the necessary props were prepared to be sent to HomeAv5R1.jsx. necessary css edits were made in the homeAv5.css file.

--- CyberV1/src/components/homeA/homeAv5/HomeAv5.jsx
+++ CyberV1/src/components/homeA/homeAv5/HomeAv5.jsx
@@ -1,25 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./homeAv5.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import HomeAv5R1 from "./homeAv5R1/HomeAv5R1";
+import axios from "axios";
 
 const HomeAv5 = () => {
+  const [productsB, setProductsB] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/productsB")
+      .then((response) => {
+        setProductsB(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-      <Container className="homeAv5Banners">
-        <Row>
-          <Col className="d-flex flex-column justify-content-center align-items-center">
-            <HomeAv5R1 />
+    <Container fluid className="homeAv5Banners">
+      <Row className="justify-content-start align-items-center homeAv5Banners-row">
+        {productsB.map((product) => (
+          <Col
+            key={product.id}
+            xs={12}
+            sm={6}
+            md={3}
+            className="homeAv5BannerCol"
+          >
+            <HomeAv5R1
+              titleBe={product.titlePb}
+              descPbE={product.descPb}
+              imgBe={product.imgB}
+              keyId={product.keyId}
+            />
           </Col>
-          <Col>1 of 1</Col>
-          <Col>1 of 1</Col>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-    </>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
--- CyberV1/src/components/homeA/homeAv5/homeAv5R1/HomeAv5R1.jsx
+++ CyberV1/src/components/homeA/homeAv5/homeAv5R1/HomeAv5R1.jsx
@@ -1,23 +1,30 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
-import "./homeAv5R1.css";
+import "./homeAv5R1.css"; // Stil dosyası
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-const HomeAv5R1 = () => {
+
+const HomeAv5R1 = ({ titleBe, descPbE, imgBe, keyId }) => {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </>
+    <Card className={`homeAv5R1Card homeAv5R1Card-${keyId}`}>
+      <Card.Img
+        variant="top"
+        src={imgBe}
+        className={`homeAv5R1Card-img homeAv5R1Card-img-${keyId}`}
+      />
+      <Card.Body className="homeAv5R1Card-bdy">
+        <Card.Title
+          className={`homeAv5R1Card-title homeAv5R1Card-title-${keyId}`}
+        >
+          {titleBe}
+        </Card.Title>
+        <Card.Text className="homeAv5R1Card-desc">{descPbE}</Card.Text>
+        <Button className={`homeAv5R1Card-btn homeAv5R1Card-btn-${keyId}`}>
+          Shop Now
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
