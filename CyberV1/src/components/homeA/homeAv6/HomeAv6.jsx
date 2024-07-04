38ecf90ece379c0ab4c61ce804cb9b7f9048d5b6
ebcengiz <enesbugracengiz@gmail.com>
2024-07-04 15:05:24 +0300

    I completed the first row in the HomeAv6.jsx file and made the necessary css edits on the homeAv6.css side.

--- CyberV1/src/components/homeA/homeAv6/HomeAv6.jsx
+++ CyberV1/src/components/homeA/homeAv6/HomeAv6.jsx
@@ -8,9 +8,14 @@ import Col from "react-bootstrap/Col";
 const HomeAv6 = () => {
   return (
     <>
-      <Container fluid className="bg-danger">
+      <Container fluid className="homeAv6-cntnr">
+        <Row className="homeAv6-rw1">
+          <Col>
+            <span className="homeAv6-spn">Discounts up to -50%</span>
+          </Col>
+        </Row>
         <Row>
-          <Col>1 of 1</Col>
+          <Col>1 of 2</Col>
         </Row>
       </Container>
     </>
