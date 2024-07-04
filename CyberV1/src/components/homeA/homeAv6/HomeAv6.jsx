b7b7b8cb3b35740ce40d67cf4b0b4552c7599681
ebcengiz <enesbugracengiz@gmail.com>
2024-07-04 16:00:34 +0300

    Data was extracted with axios in the HomeAv6.jsx file. homeAv6R.css and HomeAv6R.jsx files were created.

--- CyberV1/src/components/homeA/homeAv6/HomeAv6.jsx
+++ CyberV1/src/components/homeA/homeAv6/HomeAv6.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./homeAv6.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import axios from "axios";
 
 const HomeAv6 = () => {
+  const [productsDpE, setProductsDpE] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/productsDp")
+      .then((response) => {
+        setProductsDpE(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <Container fluid className="homeAv6-cntnr">
@@ -15,7 +29,9 @@ const HomeAv6 = () => {
           </Col>
         </Row>
         <Row>
-          <Col>1 of 2</Col>
+          {productsDpE.map((product) => (
+            <Col key={product.id} xs={12} sm={6} md={3}></Col>
+          ))}
         </Row>
       </Container>
     </>
---
+++ CyberV1/src/components/homeA/homeAv6/homeAv6R/HomeAv6R.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./homeAv6R.css";
+const HomeAv6R = () => {
+  return <></>;
+};
+
+export default HomeAv6R;
