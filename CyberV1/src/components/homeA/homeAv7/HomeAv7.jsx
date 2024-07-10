4e5d0f4a0ca6a26d421bb4c8a73235a6d350428b
ebcengiz <enesbugracengiz@gmail.com>
2024-07-10 21:50:38 +0300

    For page redirection in HomaAv7.jsx, I wrapped it with a Container-made Link tag.

--- CyberV1/src/components/homeA/homeAv7/HomeAv7.jsx
+++ CyberV1/src/components/homeA/homeAv7/HomeAv7.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import "./homeAv7.css";
 import Banner2L from "../../../../public/Banner2L.svg";
 import Container from "react-bootstrap/Container";
-
+import { Link } from "react-router-dom";
 const HomeAv7 = () => {
   return (
     <>
-      <Container fluid className="homeAv7E">
-        <img src={Banner2L} alt="Banner2L" />
-      </Container>
+      <Link to="/productsPage">
+        <Container fluid className="homeAv7E">
+          <img src={Banner2L} alt="Banner2L" />
+        </Container>
+      </Link>
     </>
   );
 };
