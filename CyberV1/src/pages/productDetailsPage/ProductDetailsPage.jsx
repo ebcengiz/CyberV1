6a834a1ef00934e504fb2e581028856d43b24062
ebcengiz <enesbugracengiz@gmail.com>
2024-07-10 23:02:39 +0300

    I created productDetailsPage.css and ProductDetailsPage.jsx files.

---
+++ CyberV1/src/pages/productDetailsPage/ProductDetailsPage.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./productDetailsPage.css";
+const ProductDetailsPage = () => {
+  return <></>;
+};
+
+export default ProductDetailsPage;
