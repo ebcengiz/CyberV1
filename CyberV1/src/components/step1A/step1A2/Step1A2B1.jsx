2026738420228766efef2b48e9c28d16425a86ec
ebcengiz <enesbugracengiz@gmail.com>
2024-07-19 18:04:49 +0300

    2 new images have been added to my project. A new div structure was created in Step1.jsx file and Step1A2B1.jsx file was integrated here. Necessary css edits were made in Step1.css file. Step1A2B1.jsx file was created and the structure in it was edited and visuals were added. necessary css edits were made in step1A2B1.css file.

---
+++ CyberV1/src/components/step1A/step1A2/Step1A2B1.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./step1A2B1.css";
+import Form from "react-bootstrap/Form";
+import Actions1 from "../../../assets/Actions.svg";
+import LineIcon1 from "../../../assets/Line+Icon.svg";
+
+const Step1A2B1 = () => {
+  return (
+    <>
+      <div className="step1A2B1-main">
+        <span className="step1A2B1-text">Select Address</span>
+        <div className="step1A2B1-main-adrs">
+          <div className="step1A2B1-main-adrs-content">
+            <div className="step1A2B1-main-adrs-content-top">
+              <Form.Check
+                type="radio"
+                aria-label="radio 1"
+                className="custom-radio"
+              />
+              <span className="step1A2B1-main-adrs-content-top-spn1">
+                2118 Thornridge
+              </span>
+              <div className="step1A2B1-main-adrs-content-top-tag">HOME</div>
+            </div>
+            <div className="step1A2B1-main-adrs-content-top-info">
+              <span className="step1A2B1-main-adrs-content-top-info-spn1">
+                2118 Thornridge Cir. Syracuse, Connecticut 35624
+              </span>
+              <span className="step1A2B1-main-adrs-content-top-info-spn1">
+                (209) 555-0104
+              </span>
+            </div>
+          </div>
+          <img
+            src={Actions1}
+            alt="Actions"
+            className="step1A2B1-main-adrs-img"
+          />
+        </div>
+        <div className="step1A2B1-main-adrs">
+          <div className="step1A2B1-main-adrs-content">
+            <div className="step1A2B1-main-adrs-content-top">
+              <Form.Check
+                type="radio"
+                aria-label="radio 1"
+                className="custom-radio"
+              />
+              <span className="step1A2B1-main-adrs-content-top-spn1">
+                Headoffice
+              </span>
+              <div className="step1A2B1-main-adrs-content-top-tag">OFFICE</div>
+            </div>
+            <div className="step1A2B1-main-adrs-content-top-info">
+              <span className="step1A2B1-main-adrs-content-top-info-spn1">
+                2715 Ash Dr. San Jose, South Dakota 83475
+              </span>
+              <span className="step1A2B1-main-adrs-content-top-info-spn1">
+                (704) 555-0127
+              </span>
+            </div>
+          </div>
+          <img
+            src={Actions1}
+            alt="Actions"
+            className="step1A2B1-main-adrs-img"
+          />
+        </div>
+        <img src={LineIcon1} alt="line+icon" className="step1A2B1-main-img" />
+        <span className="step1A2B1-main-text1">Add New Address</span>
+      </div>
+    </>
+  );
+};
+
+export default Step1A2B1;
+
+<Form.Check type="radio" aria-label="radio 1" />;
--- CyberV1/src/pages/step1/Step1.jsx
+++ CyberV1/src/pages/step1/Step1.jsx
@@ -4,6 +4,7 @@ import "./step1.css";
 import Step1A1 from "../../components/step1A/step1A1/Step1A1";
 import Step1A2 from "../../components/step1A/step1A1/Step1A2";
 import Step1A3 from "../../components/step1A/step1A1/Step1A3";
+import Step1A2B1 from "../../components/step1A/step1A2/Step1A2B1";
 const Step1 = () => {
   return (
     <>
@@ -11,6 +12,9 @@ const Step1 = () => {
         <Step1A1 />
         <Step1A2 />
         <Step1A3 />
+      </div>
+      <div className="step1-main1">
+        <Step1A2B1 />
       </div>
     </>
   );
